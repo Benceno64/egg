eaa035507cf6a181ba85c731231ab7f0ae05b5f0
benceno <benceno>
2023-03-07 17:40:11 -0300

    primer commit

---
+++ javaintroej01/src/javaintroej01/Javaintroej01.java
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintroej01;
+
+/**
+ *
+ * @author drago
+ */
+public class Javaintroej01 {
+
+    //la siguiente linea es el cuerpo de la clase con el metodo "main"
+    public static void main(String[] args) {
+        // la siguiente linea imprimira el mensaje
+    System.out.println("holamundo");
+    }
+    public static void main(String[] args) {
+        System.out.println("que onda bro?");
+    }
+}
