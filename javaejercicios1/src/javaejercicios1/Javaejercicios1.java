5c4e7eba8b8ba87033a6a3fc1da7be3110bb7ba3
benceno <benceno>
2023-03-07 18:39:22 -0300

    segundo commit

--- javaejercicios1/src/javaejercicios1/Javaejercicios1.java
+++ javaejercicios1/src/javaejercicios1/Javaejercicios1.java
@@ -1,19 +1,21 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaintroej01;
+package javaejercicios1;
 
 /**
  *
  * @author drago
  */
-public class Javaintroej01 {
+public class Javaejercicios1 {
 
-    //la siguiente linea es el cuerpo de la clase con el metodo "main"
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        // la siguiente linea imprimira el mensaje
-    System.out.println("holamundo");
+        // TODO code application logic here
     }
+    
 }
