1b0051ed2dbf49cca13cbf03d9bb58921d460f0a
benceno <benceno>
2023-03-08 21:01:49 -0300

    commit

---
+++ ejercicios java/encuentro1_1/src/encuentro1_1/Encuentro1_1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encuentro1_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author drago
+ */
+public class Encuentro1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main (String[] args){
+    
+         Scanner leer = new Scanner(System.in);
+         System.out.println("ingresa un numero entero");
+         int num1 = leer.nextInt();
+         System.out.println("ingresa el segundo numero");
+         int num2 = leer.nextInt();
+         int suma = num1 + num2
+         System.out.println("el resultado es " + suma)
+    }
+}
